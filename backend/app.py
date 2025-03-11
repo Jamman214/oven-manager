0ff38e5a36cd296365c2540b36ae26eb70c90fc9
josh turner <joshgt400@gmail.com>
2025-03-11 18:54:54 +0000

    modifed env variables access

--- backend/app.py
+++ backend/app.py
@@ -1,18 +1,8 @@
 from flask import Flask
-import os
-from dotenv import dotenv_values
 
 app = Flask(__name__)
-
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-env_path = os.path.join(BASE_DIR, '.env')
-secrets = dotenv_values(env_path)
-
-app.secret_key = secrets['APP_SECRET_KEY']
+app.config.from_prefixed_env()
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
