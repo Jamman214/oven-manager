13fb1d6765bae81be63fc250ec398152a82eef17
josh turner <joshgt400@gmail.com>
2025-03-11 04:00:59 +0000

    simple flask app

---
+++ app/main.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from dotenv import dotenv_values
+
+app = Flask(__name__)
+
+secrets = dotenv_values('.env')
+app.secret_key = secrets['APP_SECRET_KEY']
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
