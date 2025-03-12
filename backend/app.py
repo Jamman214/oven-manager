48218bd4ee1037aac553ead33084e97e7634e776
Josh Turner <joshgt400@gmail.com>
2025-03-12 16:42:51 +0000

    testing flask

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,6 @@ from flask import Flask
 app = Flask(__name__)
 app.config.from_prefixed_env()
 
-@app.route("/api/test")
+@app.route("/test", methods=['GET','POST'])
 def hello_world():
     return "API Test"
--- frontend/src/pages/Presets.tsx
+++ frontend/src/pages/Presets.tsx
@@ -1,5 +1,39 @@
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import React, { useState, useEffect } from "react";
+
 function Presets() {
-    return <p>Presets</p>;
+    const [text, setText] = useState("Waiting");
+    const [shouldFetch, setShouldFetch] = useState(false);
+
+    useEffect(() => {
+        if (shouldFetch) {
+            fetch("/api/test")
+                .then((res) => res.text())
+                .then((data) => {
+                    setText(data);
+                });
+        }
+    }, [shouldFetch]);
+
+    const handleClick = () => {
+        setShouldFetch(true);
+    };
+
+    return (
+        <Card style={{ width: "18rem" }}>
+            <Card.Body>
+                <Card.Title>{text}</Card.Title>
+                <Card.Text>
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary" onClick={handleClick}>
+                    Go somewhere
+                </Button>
+            </Card.Body>
+        </Card>
+    );
 }
 
 export default Presets;
