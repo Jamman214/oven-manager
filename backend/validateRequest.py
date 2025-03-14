4369f023b9f468d9d30ea2e49fb26a2394d99ed0
Josh Turner <joshgt400@gmail.com>
2025-03-14 13:49:51 +0000

    fixed typing for json validation

--- backend/validateRequest.py
+++ backend/validateRequest.py
@@ -1,101 +1,105 @@
 from collections.abc import Iterable
-from typing import Callable, TypeVar, Optional, TypeAlias, Type, overload, Any
+from typing import Callable, TypeVar, Optional, TypeAlias, Type, overload, Any, get_args, Generic, Union
+from types import NoneType
 from flask import Request
+from abc import ABC, abstractmethod
 
-# Received JSON types
-AtomicJsonNode: TypeAlias = str | int | float | bool | None 
-AJNode = TypeVar('AJNode', bound=AtomicJsonNode)
-JsonNode: TypeAlias = AtomicJsonNode | list | dict
 
-# Expected JSON types
-JsonSchemaNode: TypeAlias = Type[AtomicJsonNode] | 'ListSchema' | 'DictSchema'
-JsonSchemaNodeExtended: TypeAlias = JsonSchemaNode | 'ConstraintSchema'
+JsonSAN = TypeVar('JsonSAN', str, float, int, bool, None) # Json Schema Atomic Node
+JsonSCN = TypeVar('JsonSCN', bound = Union['ListSchema','DictSchema']) # Json Schema Compound Node
+JsonNode: TypeAlias = Type[JsonSAN] | JsonSCN
+JsonNodeConstrained: TypeAlias = JsonNode | 'ConstraintSchema'      
 
 T = TypeVar('T')
 Predicate: TypeAlias = Callable[[T],bool]
+def tautology(x: Any) -> bool:
+    return True
 
-# If valid: True, None
-# Otherwise: False, error_message
 ValidationResult : TypeAlias = tuple[bool, Optional[str]]
 
 
-class ConstraintSchema:
+class JsonSchema(ABC):
+    @abstractmethod
+    def validate(self, json_node: Any) -> ValidationResult:
+        pass
+        
+
+class ConstraintSchema(JsonSchema):
     # Used to introduce constraints on the child node
     # Should not place constraints on the children of the child node
-    
     @overload
-    def __init__(self, schema_node: 'ListSchema', filter: Predicate[list]) -> None:
+    def __init__(self, child_node: 'DictSchema', optional: bool = False, filter: Predicate[dict] = tautology) -> None:
         ...
     
     @overload
-    def __init__(self, schema_node: 'DictSchema', filter: Predicate[dict]) -> None:
-        ...
-        
-    @overload
-    def __init__(self, schema_node: Type[AJNode], filter: Predicate[AJNode]) -> None:
+    def __init__(self, child_node: 'ListSchema', optional: bool = False, filter: Predicate[list] = tautology) -> None:
         ...
     
-    def __init__(self, schema_node: JsonSchemaNode, filter: Predicate[Any]) -> None:
-        self.schema_node = schema_node
+    @overload
+    def __init__(self, child_node: Type[JsonSAN], optional: bool = False, filter: Predicate[JsonSAN] = tautology) -> None:
+        ...
+    
+    def __init__(self, child_node: JsonNode = None, optional: bool = False, filter: Predicate[Any] = tautology) -> None:
+        self.child_node = child_node
+        self.optional = optional
         self.filter = filter
-    
-    def validate(self, json_node: JsonNode) -> ValidationResult:
-        # Validate type of node  
-        valid, error_message = compare_json_types(self.schema_node, json_node)
-        # Validate value of node
+
+    def validate(self, json_node: Any) -> ValidationResult:
+        if self.optional and json_node is None:
+            return True, None
+
+        valid, error_message = compare_json_types(self.child_node, json_node)
+
         if valid and not self.filter(json_node):
-            return False, f"Got member: {json_node} which did not pass filter" 
+            return False, f"Got value: {json_node} which did not pass filter" 
         return valid, error_message
 
 
-class ListSchema:
+class ListSchema(JsonSchema):
     # Used to represent a JSON array
-    
-    def __init__(self, schema_node: JsonSchemaNodeExtended) -> None:
-        self.schema_node = schema_node
+    def __init__(self, child_node: JsonNodeConstrained):
+        self.child_node = child_node
         
-    def validate(self, json_list: JsonNode) -> ValidationResult:
-        # validate type of node
-        if type(json_list) != list:
-            return False, f"Received type: {type(json_list)}, expected type: list"
-        
-        # validate contents of node
+    def validate(self, json_node: Any) -> ValidationResult:
+        if type(json_node) != list:
+            return False, f"Received type: {type(json_node)}, expected type: list"
+        return self.validate_list_elements(json_node)
+
+    def validate_list_elements(self, json_list: list):
         for json_node in json_list:
-            valid, error_message = compare_json_types(self.schema_node, json_node)
+            valid, error_message = compare_json_types(self.child_node, json_node)
             if not valid:
                 return False, error_message
         return True, None
 
 
-class DictSchema:
+class DictSchema(JsonSchema):
     # Used to represent a JSON object
+    def __init__(self, schema: dict[str, JsonNodeConstrained]):
+        self.schema = schema
     
-    def __init__(self, schema_nodes: dict[str,JsonSchemaNodeExtended]) -> None:
-        self.schema_nodes = schema_nodes
+    def validate(self, json_node: Any) -> ValidationResult:
+        if type(json_node) != dict:
+            return False, f"Received type: {type(json_node)}, expected type: dict"
+        return self.validate_key_value_pairs(json_node)
     
-    def validate(self, json_dict: JsonNode) -> ValidationResult:
-        # validate type of node
-        if type(json_dict) != dict:
-            return False, f"Received type: {type(json_dict)}, expected type: dict"
-        
-        # validate contents of node
-        for key, schema_node in self.schema_nodes.items():
+    def validate_key_value_pairs(self, json_dict: dict) -> ValidationResult:
+        for key, schema_node in self.schema.items():
             json_node = json_dict.get(key)
             return compare_json_types(schema_node, json_node)
         return True, None
 
 
-def compare_json_types(schema_node: JsonSchemaNodeExtended, json_node: JsonNode) -> ValidationResult:    
-    if isinstance(schema_node, (ConstraintSchema, ListSchema, DictSchema)):
+def compare_json_types(schema_node: JsonNodeConstrained, json_node: Any) -> ValidationResult:    
+    if isinstance(schema_node, JsonSchema):
         return schema_node.validate(json_node)
-
-    elif schema_node == type(json_node):
+    elif type(json_node) == schema_node:
         return True, None
     
     return False, f"Expected type {schema_node}, got type {type(json_node)}"
 
 
-def validate_json_request(request: Request, schema_node: JsonSchemaNode) -> ValidationResult:
+def validate_json_request(schema_node: JsonNodeConstrained, request: Request) -> ValidationResult:
     if not request.is_json:
         return False, "Expected JSON"
     return compare_json_types(schema_node, request.get_json())
