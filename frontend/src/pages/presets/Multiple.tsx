5b8babe4618cb56edac53f4a1ca5744ddbe0f505
Josh Turner <joshgt400@gmail.com>
2025-03-23 00:49:39 +0000

    implemented multiple preset page with some validation

--- frontend/src/pages/presets/Multiple.tsx
+++ frontend/src/pages/presets/Multiple.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { number, z } from "zod";
 
-import { useForm } from "react-hook-form";
+import { useForm, useFieldArray, Controller } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
@@ -14,56 +14,188 @@ import Col from "react-bootstrap/Col";
 import Dropdown from "../../components/Dropdown";
 
 interface Item {
-    id: string;
+    id: number;
     name: string;
 }
 
 function PresetMultiple() {
     const [fetched, setFetched] = useState<boolean>(false);
     const [data, setData] = useState<Item[]>([]);
-    const [count, setCount] = useState<number>(0);
+    const [isSubmitting, setSubmitting] = useState<boolean>(false);
 
-    const decrementCount = () => {
-        if (count > 0) {
-            setCount(count - 1);
-        }
+    const dropdownSchema = z.coerce
+        .number()
+        .refine((val) => val >= 0, { message: "Select a preset", path: [] });
+
+    // Define the schema for a single time entry
+    const presetSchema = z.object({
+        time: z
+            .string()
+            .transform((time) => {
+                const match = time.match(/^(\d{2}):(\d{2})$/);
+                if (match === null) {
+                    return undefined;
+                }
+                return {
+                    hour: parseInt(match[1], 10),
+                    minute: parseInt(match[2], 10),
+                };
+            })
+            .refine((val) => !isSubmitting || val !== undefined, {
+                message: "Invalid time format",
+                path: [],
+            })
+            .refine(
+                (val) => {
+                    if (isSubmitting) {
+                        return true;
+                    }
+                    if (val === undefined) {
+                        return true;
+                    }
+                    console.log(val);
+                    if (val.hour > 23 || val.hour < 0) {
+                        return false;
+                    }
+                    if (val.minute > 59 || val.minute < 0) {
+                        return false;
+                    }
+                    return true;
+                },
+                { message: "Invalid time", path: [] }
+            ),
+        dropdown: dropdownSchema,
+    });
+
+    // Define the form schema with validation
+    const formSchema = z.object({
+        firstPreset: dropdownSchema,
+        followingPresets: z.array(presetSchema),
+    });
+
+    // Define TypeScript types based on the schema
+    type FormValues = z.infer<typeof formSchema>;
+
+    const {
+        register,
+        control,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<FormValues>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            firstPreset: -1,
+            followingPresets: [],
+        },
+        mode: "onChange",
+    });
+
+    const { fields, append, remove } = useFieldArray({
+        control,
+        name: "followingPresets",
+    });
+
+    const onSubmit = (data: FormValues) => {
+        console.log("Form submitted:", data);
+    };
+
+    const handleSubmitWrapper = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        setSubmitting(true);
+        setTimeout(() => {
+            handleSubmit(onSubmit)(e);
+            setSubmitting(false);
+        }, 0);
     };
 
     return (
-        <Form>
-            <Form.Group className="mb-3">
+        <Form onSubmit={handleSubmitWrapper} noValidate>
+            <Form.Group className="mb-3 mt-3">
                 <Row>
-                    <Col xs={12} md={{ span: 6, offset: 3 }}>
-                        {Dropdown({
-                            route: "/api/get-presets/single",
-                            setFinished: setFetched,
-                            setData: setData,
-                            required: true,
-                        })}
+                    <Col className="mb-3" xs={12} md={{ span: 6, offset: 3 }}>
+                        <Controller
+                            control={control}
+                            name="firstPreset"
+                            render={({ field }) => (
+                                <Dropdown
+                                    route="/api/get-presets/single"
+                                    returnFinished={setFetched}
+                                    returnData={setData}
+                                    required={false}
+                                    registerData={{
+                                        value: field.value,
+                                        onChange: field.onChange,
+                                        onBlur: field.onBlur,
+                                    }}
+                                />
+                            )}
+                        />
+                        {errors.firstPreset && (
+                            <Alert variant="danger">
+                                {errors.firstPreset.message}
+                            </Alert>
+                        )}
                     </Col>
                 </Row>
 
-                {Array.from({ length: count }, (_, i) => {
+                {fields.map((field2, index) => {
                     return (
-                        <Row key={i}>
-                            <Col xs={12} md={{ span: 6, offset: 3 }}>
+                        <Row key={field2.id}>
+                            <Col
+                                className="mb-3"
+                                xs={12}
+                                md={{ span: 6, offset: 3 }}
+                            >
                                 <FloatingLabel
-                                    controlId={"floatingInput-"}
+                                    controlId={`floatingInput-time-${index}`}
                                     label="End Time"
-                                    className="mb-3 text-center"
                                 >
                                     <Form.Control
                                         type="time"
                                         placeholder="HH:MM"
-                                        required
+                                        required={true}
+                                        {...register(
+                                            `followingPresets.${index}.time`
+                                        )}
                                     />
+                                    {errors.followingPresets?.[index]?.time && (
+                                        <Alert variant="danger">
+                                            {
+                                                errors.followingPresets[index]
+                                                    ?.time?.message
+                                            }
+                                        </Alert>
+                                    )}
                                 </FloatingLabel>
                             </Col>
-                            <Col xs={12} md={{ span: 6, offset: 3 }}>
-                                {Dropdown({
-                                    data: data,
-                                    required: true,
-                                })}
+                            <Col
+                                className="mb-3"
+                                xs={12}
+                                md={{ span: 6, offset: 3 }}
+                            >
+                                <Controller
+                                    control={control}
+                                    name={`followingPresets.${index}.dropdown`}
+                                    render={({ field }) => (
+                                        <Dropdown
+                                            data={data}
+                                            required={true}
+                                            registerData={{
+                                                value: field.value,
+                                                onChange: field.onChange,
+                                                onBlur: field.onBlur,
+                                            }}
+                                        />
+                                    )}
+                                />
+                                {errors.followingPresets?.[index]?.dropdown && (
+                                    <Alert variant="danger">
+                                        {
+                                            errors.followingPresets[index]
+                                                ?.dropdown?.message
+                                        }
+                                    </Alert>
+                                )}
                             </Col>
                         </Row>
                     );
@@ -71,7 +203,12 @@ function PresetMultiple() {
             </Form.Group>
             <Row>
                 <Col xs={3} md={4} className="d-flex justify-content-end">
-                    <Button type="button" onClick={decrementCount}>
+                    <Button
+                        type="button"
+                        onClick={() => {
+                            remove(fields.length - 1);
+                        }}
+                    >
                         -
                     </Button>
                 </Col>
@@ -82,7 +219,10 @@ function PresetMultiple() {
                     <Button
                         type="button"
                         onClick={() => {
-                            setCount(count + 1);
+                            append(
+                                { time: undefined, dropdown: -1 },
+                                { shouldFocus: false }
+                            );
                         }}
                     >
                         +
