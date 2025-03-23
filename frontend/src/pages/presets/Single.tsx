412f75f3e4ad4510dbf773f94d910ba52c6ace02
Josh Turner <joshgt400@gmail.com>
2025-03-23 17:19:45 +0000

    Refactored code, and hid invalid temp errors unless the form was submitted, also changed to keep errors shown until input is changed

--- frontend/src/pages/presets/Single.tsx
+++ frontend/src/pages/presets/Single.tsx
@@ -11,88 +11,108 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
-function getSchema() {
-    const minTemp = 0;
-    const maxTemp = 500;
-
-    const numSchema = z.preprocess(
-        (val) => (Number.isNaN(val) ? undefined : Number(val)), // Convert empty string to undefined
-        z.number().min(minTemp, "Too low").max(maxTemp, "Too high").optional()
-    );
-
-    return z
-        .object({
-            coreHigh: numSchema,
-            coreLow: numSchema,
-            ovenHigh: numSchema,
-            ovenLow: numSchema,
-        })
-        .superRefine((data, ctx) => {
-            if (data.coreLow && data.coreHigh && data.coreLow > data.coreHigh) {
-                ctx.addIssue({
-                    code: z.ZodIssueCode.custom,
-                    message: `Core High Temp must be greater than Core Low Temp.`,
-                    path: ["coreHigh"],
-                });
-                ctx.addIssue({
-                    code: z.ZodIssueCode.custom,
-                    message: `Core Low Temp must be lower than Core High Temp.`,
-                    path: ["coreLow"],
-                });
-            }
-        })
-        .superRefine((data, ctx) => {
-            if (data.ovenLow && data.ovenHigh && data.ovenLow > data.ovenHigh) {
-                ctx.addIssue({
-                    code: z.ZodIssueCode.custom,
-                    message: `Oven High Temp must be greater than Oven Low Temp.`,
-                    path: ["ovenHigh"],
-                });
-                ctx.addIssue({
-                    code: z.ZodIssueCode.custom,
-                    message: `Oven Low Temp must be lower than Oven High Temp.`,
-                    path: ["ovenLow"],
-                });
-            }
-        })
-        .superRefine((data, ctx) => {
-            if (data.ovenHigh && data.coreLow && data.ovenHigh > data.coreLow) {
-                ctx.addIssue({
-                    code: z.ZodIssueCode.custom,
-                    message: `Core temperature must be greater than Oven temperature.`,
-                    path: ["coreLow"],
-                });
-                ctx.addIssue({
-                    code: z.ZodIssueCode.custom,
-                    message: `Oven temperature must be lower than Core temperature.`,
-                    path: ["ovenHigh"],
-                });
-            }
-        });
-}
-
 function capitalise(word: string) {
     return word.charAt(0).toUpperCase() + word.slice(1);
 }
 
 function PresetSingle() {
+    // Used to prevent error messages for blank inputs when not submitting (I think they're annoying)
+    const [isSubmitting, setSubmitting] = useState<boolean>(false);
+    const [formEvent, setFormEvent] =
+        useState<React.FormEvent<HTMLFormElement>>();
+
+    // Constants for accessing inputs
     const sectors = ["core", "oven"] as const;
-    type Sector = (typeof sectors)[number];
+    const limits = ["high", "low"] as const;
 
-    const limits = ["High", "Low"] as const;
-    type Limit = (typeof limits)[number];
-
-    type SectorLimit = `${Sector}${Limit}`;
-    const sectorLimits = [
-        "coreHigh",
-        "coreLow",
-        "ovenHigh",
-        "ovenLow",
-    ] as SectorLimit[];
-
-    const combineSectorLimit = (sector: Sector, limit: Limit): SectorLimit => {
-        return `${sector}${limit}`;
+    // Used to individually control submitting status for each input
+    // Without this, when updating paired error messages, it would remove messages produced when submitting
+    // i.e. The Invalid Temperature message
+    const submitMap = () => {
+        const [isSubmittingVal, setSubmittingVal] = useState<boolean>(false);
+        return { val: isSubmittingVal, set: setSubmittingVal };
     };
+    const isSubmittings = {
+        core: { high: submitMap(), low: submitMap() },
+        oven: { high: submitMap(), low: submitMap() },
+    };
+
+    // Schema for each individual input, paired to its submit status to prevent certain errors appearing
+    // before submissions, and to prevent those same messages dissapearing after submission before changes
+    // are made to the input field
+    const numSchema = (sector: "core" | "oven", limit: "high" | "low") =>
+        z
+            .preprocess(
+                (val) => (Number.isNaN(val) ? undefined : Number(val)), // Convert empty string to undefined
+                z
+                    .number()
+                    .min(0, "Temperature cannot be below 0°C.")
+                    .max(500, "Temperature cannot be above 500°C.")
+                    .optional()
+            )
+            .refine(
+                (val) => {
+                    console.log(isSubmittings[sector][limit], val);
+                    return (
+                        !isSubmittings[sector][limit].val || val !== undefined
+                    );
+                },
+                {
+                    message: "Temperature is invalid",
+                    path: [],
+                }
+            );
+
+    // Schema which enforces high temp being above low temp
+    const sectorSchema = (sector: "core" | "oven") => {
+        return z
+            .object({
+                high: numSchema(sector, "high"),
+                low: numSchema(sector, "low"),
+            })
+            .superRefine((data, ctx) => {
+                if (data.low && data.high && data.low >= data.high) {
+                    ctx.addIssue({
+                        code: z.ZodIssueCode.custom,
+                        message: `High Temp must be above Low Temp.`,
+                        path: ["high"],
+                    });
+                    ctx.addIssue({
+                        code: z.ZodIssueCode.custom,
+                        message: `Low Temp must be below High Temp.`,
+                        path: ["low"],
+                    });
+                }
+            });
+    };
+
+    // Schema which enforces core temp being above oven temp
+    const formSchema = z
+        .object({
+            core: sectorSchema("core"),
+            oven: sectorSchema("oven"),
+        })
+        .superRefine((data, ctx) => {
+            if (
+                data.core.low &&
+                data.oven.high &&
+                data.core.low < data.oven.high
+            ) {
+                ctx.addIssue({
+                    code: z.ZodIssueCode.custom,
+                    message: `Core Low Temp cannot be below Oven High Temp.`,
+                    path: ["core", "low"],
+                });
+                ctx.addIssue({
+                    code: z.ZodIssueCode.custom,
+                    message: `Oven High Temp cannot be above Core Low Temp.`,
+                    path: ["oven", "high"],
+                });
+            }
+        });
+
+    // Define TypeScript types based on the schema
+    type FormValues = z.infer<typeof formSchema>;
 
     const {
         register,
@@ -100,53 +120,64 @@ function PresetSingle() {
         formState: { errors },
         watch,
         trigger,
-    } = useForm({
-        resolver: zodResolver(getSchema()),
+    } = useForm<FormValues>({
+        resolver: zodResolver(formSchema),
         mode: "onChange",
     });
 
-    const coreHighValue = watch("coreHigh");
-    useEffect(() => {
-        trigger("coreLow");
-        console.log(coreHighValue);
-    }, [coreHighValue, trigger]);
+    // Update error messages in each sector when other limit updates
+    for (const sector of sectors) {
+        for (let i = 0; i < 2; i++) {
+            const watcher = watch(`${sector}.${limits[i]}`);
 
-    const coreLowValue = watch("coreLow");
-    useEffect(() => {
-        trigger("coreHigh");
-        trigger("ovenHigh");
-    }, [coreLowValue, trigger]);
+            useEffect(() => {
+                trigger(`${sector}.${limits[(i + 1) % 2]}`);
+            }, [watcher, trigger]);
+        }
+    }
 
-    const ovenHighValue = watch("ovenHigh");
+    // Update error messages between sectors when they overlap
     useEffect(() => {
-        trigger("ovenLow");
-        trigger("coreLow");
-    }, [ovenHighValue, trigger]);
+        trigger("oven.high");
+    }, [watch("core.low"), trigger]);
 
-    const ovenLowValue = watch("ovenLow");
     useEffect(() => {
-        trigger("ovenHigh");
-    }, [ovenLowValue, trigger]);
+        trigger("core.low");
+    }, [watch("oven.high"), trigger]);
 
-    const onSubmit = (data: {
-        coreHigh?: number;
-        coreLow?: number;
-        ovenHigh?: number;
-        ovenLow?: number;
-    }) => {
+    // Sets isSubmitting to true globally and for all elements
+    const handleSubmitWrapper = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        setFormEvent(e);
+        isSubmittings["core"]["high"].set(true);
+        isSubmittings["core"]["low"].set(true);
+        isSubmittings["oven"]["high"].set(true);
+        isSubmittings["oven"]["low"].set(true);
+        setSubmitting(true);
+    };
+
+    // Function to run on success
+    const onSubmit = (data: FormValues) => {
         console.log("Submitted data:", data);
     };
 
-    const handleSubmitWrapper = (e: React.FormEvent<HTMLFormElement>) => {
-        handleSubmit(onSubmit)(e);
-    };
+    // Attempts to submit form
+    useEffect(() => {
+        if (!isSubmitting) {
+            return;
+        }
+        console.log("issubmitting: ", isSubmitting);
+        handleSubmit(onSubmit)(formEvent);
+        setSubmitting(false);
+    }, [isSubmitting]);
 
-    const temperatureInputs = (sector: Sector) => {
-        return limits.map((limit: Limit, i) => {
-            const field = combineSectorLimit(sector, limit); // Combine sector and limit into a single type
-            const fieldError = errors[field];
+    // Web page
+    const limitInputs = (sector: "core" | "oven", i: number) => {
+        return limits.map((limit, j) => {
+            const field = `${sector}.${limit}`; // Combine sector and limit into a single type
+            const fieldError = errors?.[sector]?.[limit]; // Get possibly undefined key
             return (
-                <Col md={i % 2 === 0 ? { span: 3, offset: 3 } : 3} key={i}>
+                <Col md={j % 2 === 0 ? { span: 3, offset: 3 } : 3} key={j}>
                     <FloatingLabel
                         controlId={"floatingInput-" + field}
                         label={limit}
@@ -155,9 +186,17 @@ function PresetSingle() {
                         <Form.Control
                             type="number"
                             placeholder=" "
-                            {...register(field, { valueAsNumber: true })}
+                            {...register(`${sector}.${limit}`, {
+                                valueAsNumber: true,
+                            })}
                             isInvalid={!!fieldError}
-                            required
+                            onChange={(e) => {
+                                // Call the existing registered onChange handler
+                                register(`${sector}.${limit}`).onChange(e);
+
+                                // Your additional action
+                                isSubmittings[sector][limit].set(false);
+                            }}
                         />
 
                         {fieldError && (
@@ -169,24 +208,26 @@ function PresetSingle() {
         });
     };
 
-    const coreAndOvenInputs = sectors.map((sector: Sector, i) => {
-        return (
-            <Form.Group className="mb-3" key={i}>
-                <Row>
-                    <Col md={{ span: 6, offset: 3 }}>
-                        <Form.Label>
-                            {capitalise(sector)} Temperature
-                        </Form.Label>
-                    </Col>
-                </Row>
-                <Row>{temperatureInputs(sector)}</Row>
-            </Form.Group>
-        );
-    });
+    const sectorInputs = (() => {
+        return sectors.map((sector, i) => {
+            return (
+                <Form.Group className="mb-3" key={i}>
+                    <Row>
+                        <Col md={{ span: 6, offset: 3 }}>
+                            <Form.Label>
+                                {capitalise(sector)} Temperature
+                            </Form.Label>
+                        </Col>
+                    </Row>
+                    <Row>{limitInputs(sector, i)}</Row>
+                </Form.Group>
+            );
+        });
+    })();
 
     return (
-        <Form onSubmit={handleSubmitWrapper}>
-            {coreAndOvenInputs}
+        <Form onSubmit={handleSubmitWrapper} noValidate>
+            {sectorInputs}
             <Row className="d-flex justify-content-center">
                 <Col xs={6} className="text-center">
                     <Button type="submit">Save Preset</Button>
