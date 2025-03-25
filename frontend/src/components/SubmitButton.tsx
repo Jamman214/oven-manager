cac10d92bf2562888297de9d06d3d2f0e6155c85
Josh Turner <joshgt400@gmail.com>
2025-03-25 23:21:39 +0000

    modified to use context instead of returning an object

--- frontend/src/components/SubmitButton.tsx
+++ frontend/src/components/SubmitButton.tsx
@@ -1,5 +1,55 @@
+import React, {
+    createContext,
+    useState,
+    useContext,
+    ReactNode,
+    useReducer,
+    useEffect,
+} from "react";
 import Button from "react-bootstrap/Button";
-import { useState, ButtonHTMLAttributes, useEffect } from "react";
+
+enum SubmitAction {
+    RESET = "RESET",
+    SUBMIT = "SUBMITTING",
+    SUCCEED = "SUCCESS",
+    FAIL = "FAIL",
+}
+
+// Create a context for form submission state and handling
+const ResponsiveSubmissionContext = createContext<
+    | {
+          submitState: SubmitAction;
+          dispatch: React.Dispatch<React.SetStateAction<SubmitAction>>;
+      }
+    | undefined
+>(undefined);
+
+// Custom hook to use the form submission context
+const useResponsiveSubmission = () => {
+    const context = useContext(ResponsiveSubmissionContext);
+    if (!context) {
+        throw new Error(
+            "useResponsiveSubmission must be used within a ResponsiveSubmissionProvider"
+        );
+    }
+    return context;
+};
+
+const ResponsiveSubmissionProvider = ({
+    children,
+}: {
+    children?: ReactNode;
+}) => {
+    const [submitState, dispatch] = useState<SubmitAction>(SubmitAction.RESET);
+
+    return (
+        <ResponsiveSubmissionContext.Provider
+            value={{ submitState: submitState, dispatch: dispatch }}
+        >
+            {children}
+        </ResponsiveSubmissionContext.Provider>
+    );
+};
 
 interface Props {
     text?: {
@@ -26,22 +76,24 @@ function SubmitButton({ text = {}, ...buttonProps }: SubmitButtonProps) {
     const [disabled, setDisabled] = useState<boolean>(false);
     const [countdown, setCountdown] = useState<boolean>(false);
 
-    const setInitial = () => {
+    const { submitState } = useResponsiveSubmission();
+
+    const reset = () => {
         setButtonText(initial);
         setDisabled(false);
     };
 
-    const setSubmitting = () => {
+    const submit = () => {
         setButtonText(submitting);
         setDisabled(true);
     };
 
-    const setSuccess = () => {
+    const succeed = () => {
         setButtonText(success);
         setCountdown(true);
     };
 
-    const setError = (specificError = null) => {
+    const fail = (specificError = null) => {
         setButtonText(specificError || error);
         setCountdown(true);
     };
@@ -51,21 +103,38 @@ function SubmitButton({ text = {}, ...buttonProps }: SubmitButtonProps) {
             return;
         }
         setTimeout(() => {
-            setInitial();
+            reset();
             setCountdown(false);
         }, delay);
     }, [countdown]);
 
-    return {
-        button: (
-            <Button type="submit" disabled={disabled} {...buttonProps}>
-                {buttonText}
-            </Button>
-        ),
-        setToSubmit: setSubmitting,
-        setToSuccess: setSuccess,
-        setToError: setError,
-    };
+    useEffect(() => {
+        switch (submitState) {
+            case SubmitAction.RESET:
+                reset();
+                break;
+            case SubmitAction.SUBMIT:
+                submit();
+                break;
+            case SubmitAction.SUCCEED:
+                succeed();
+                break;
+            case SubmitAction.FAIL:
+                fail();
+                break;
+        }
+    }, [submitState]);
+
+    return (
+        <Button type="submit" disabled={disabled} {...buttonProps}>
+            {buttonText}
+        </Button>
+    );
 }
 
-export default SubmitButton;
+export {
+    SubmitAction,
+    SubmitButton,
+    useResponsiveSubmission,
+    ResponsiveSubmissionProvider,
+};
