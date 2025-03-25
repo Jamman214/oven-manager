52edaef95235cfcf3740eb29f67ab804c7fca05b
Josh Turner <joshgt400@gmail.com>
2025-03-25 23:18:48 +0000

    added backend route for single preset

--- backend/app.py
+++ backend/app.py
@@ -11,6 +11,36 @@ def get_presets_single():
 
 
 
+@app.route("/set-preset/single", methods=["POST"])
+def set_preset_single():
+    min_temp = 0
+    max_temp = 500
+   
+    temperature_schema = ConstraintSchema(int, filter=lambda x : (x >= min_temp) and (x <= max_temp))
+    
+    limit_schema = ConstraintSchema(
+        DictSchema({
+            'high': temperature_schema,
+            'low':temperature_schema
+        }),
+        filter = lambda x: x['high'] > x['low']
+    )
+
+    schema = ConstraintSchema(
+        DictSchema({
+            'core': limit_schema,
+            'oven': limit_schema
+        }),
+        filter = lambda x: x['core']['low'] >= x['oven']['high']
+    )
+    
+    valid, error_message = validate_json_request(schema, request)
+    
+    sleep(2)
+    
+    if not valid:
+        return error_message, 400
+    return "Success", 200
 
 
 
