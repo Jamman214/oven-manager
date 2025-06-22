c4b7e92ae7bd926db657d1e54dbd50046af67bb3
josh turner <joshgt400@gmail.com>
2025-06-22 23:18:08 +0000

    defined breakpoints for tsx

---
+++ frontend/src/utility/breakpoints.tsx
@@ -0,0 +1,11 @@
+// Small tablets and large smartphones (landscape view)
+const breakpoint_sm = 576;
+
+// Small tablets (portrait view)
+const breakpoint_md = 768;
+
+// Tablets and small desktops
+const breakpoint_lg = 992;
+
+// Large tablets and desktops
+const breakpoint_xl = 1200;
